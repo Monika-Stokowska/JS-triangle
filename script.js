fc79586656dd57e5a118b5cf46b15e6c3a8409b4
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2017-10-05 11:24:52 +0200

    praca domowa triangle

---
+++ script.js
@@ -0,0 +1,6 @@
+var a = prompt("Enter value a");
+var h = prompt("Enter value h");
+var triangleArea = a*h/2;
+console.log("Triangle field with base a: " + a + " and height h: " + h + " is equal to: " + triangleArea);
+
+
